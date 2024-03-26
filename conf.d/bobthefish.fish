af7b79872cda3eab4291f6276ea7c8e513ea9b23
Tri Pham <techcaotri@gmail.com>
2024-03-26 16:41:20 +0700

    Fix git workspace not showing information & icons in bobthefish prompt

--- conf.d/bobthefish.fish
+++ conf.d/bobthefish.fish
@@ -4,6 +4,7 @@ set -g theme_display_user ssh
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt "➜ "
 set -g fish_prompt_pwd_dir_length 2
+set -g theme_git_worktree_support yes
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
 set -g theme_display_git_untracked yes
