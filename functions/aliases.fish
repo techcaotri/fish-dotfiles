15cf58bc3195e89d4b7474cc4191788f7fc20659
Tri Pham <techcaotri@gmail.com>
2024-03-27 14:20:39 +0700

    Add aliases for Flutter development; Add 'kenji-miyake/reload.fish' plugin; Add 'gazorby/fish-abbreviation-tips' plugin

---
+++ functions/aliases.fish
@@ -0,0 +1,4 @@
+alias flcp="bass source ~/.dotfiles/bash/aliases.sh \\; flcp"
+alias flcpp="bass source ~/.dotfiles/bash/aliases.sh \\; flcpp"
+alias fldcp="bass source ~/.dotfiles/bash/aliases.sh \\; fldcp"
+alias fldcpp="bass source ~/.dotfiles/bash/aliases.sh \\; fldcpp"
