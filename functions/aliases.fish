f8b92652d6b51c03d700778cc62812299dbfc4a5
Tri Pham <techcaotri@gmail.com>
2024-04-08 22:55:28 +0700

    Add aliases for fkill

--- functions/aliases.fish
+++ functions/aliases.fish
@@ -3,3 +3,6 @@ alias flcpp="bass source ~/.dotfiles/bash/aliases.sh \\; flcpp"
 alias fldcp="bass source ~/.dotfiles/bash/aliases.sh \\; fldcp"
 alias fldcpp="bass source ~/.dotfiles/bash/aliases.sh \\; fldcpp"
 alias tmux_send_command_to_every_pane="bass source ~/.dotfiles/bash/aliases.sh \\; tmux_send_command_to_every_pane"
+
+# fzf aliases
+alias fkill="bass source ~/.fzf-extras/fzf-extras.sh \\; fkill"
