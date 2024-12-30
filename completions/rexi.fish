5093c20519f331727edda29c12c1077c2032abe3
Tri Pham <techcaotri@gmail.com>
2024-12-30 16:34:13 +0700

    Add completions for 'rexi'

---
+++ completions/rexi.fish
@@ -0,0 +1,1 @@
+complete --command rexi --no-files --arguments "(env _REXI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) rexi)" --condition "env _REXI_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) rexi"
